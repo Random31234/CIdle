c193d87a5d741e05e84e608fd6ce4b580c8ad7c8
Random31234 <randorte3@gmail.com>
2025-02-24 09:39:04 -0800

    Value system basics are done

--- Scripts/ButtonUnitContainer.gd
+++ Scripts/ButtonUnitContainer.gd
@@ -1,18 +1,19 @@
 extends BoxContainer
-
-signal Value
+class_name ButtonUnitManager
+signal Value(i:int)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	for x in get_children():
 		
 		x.get_child(0).connect("button_down",valuePass.bind(x.get_index()))
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
 func valuePass(i:int):
 	print(i)
 	print("pressed")
+	emit_signal("Value",i)
+	return i
--- Scripts/MainCycle.gd
+++ Scripts/MainCycle.gd
@@ -1,7 +1,7 @@
 extends Control
 
-
-
+@export var calories:ButtonUnitManager
+@export var capacity:ButtonUnitManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +11,17 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+
+
+#this handles buying calories items
+func _on_calories_value(i: int) -> void:
+	print(i)
+	
+
+
+#this handles buying capacity items.
+
+func _on_capacity_value(i: int) -> void:
+	print(i)
+	print("ca")
