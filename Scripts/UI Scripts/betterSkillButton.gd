e10186cf72afa7a4951182f7e857ff8e60b41373
Random31234 <randorte3@gmail.com>
2025-08-21 14:50:20 -0700

    Changes made

--- Scripts/UI Scripts/betterSkillButton.gd
+++ Scripts/UI Scripts/betterSkillButton.gd
@@ -2,5 +2,25 @@ extends Button
 
 class_name BetterSkillButton
 
-
+@onready var panel = $Panel
 @onready var label = $MarginContainer/Label
+@export var limit:int
+@export var cost:Big
+@export var scaler:Big
+
+
+func _ready() -> void:
+	addLevel()
+
+@export var level: int = 0:
+	set(value):
+		if(value <= limit):
+			level = value
+		label.text = str(level) + "/" + str(limit)
+	
+	
+
+
+func addLevel():
+	level +=1
+	
